2bee078ff00a62e52704ac7d1a68eeda3f11a2b9
Ana Matei <anamatei.099@gmail.com>
2022-03-28 14:57:24 +0100

    Updates to Navbar

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,4 +1,12 @@
-import { Container, Navbar, Row, Col } from "react-bootstrap";
+import {
+  Container,
+  Navbar,
+  Row,
+  Col,
+  ButtonGroup,
+  DropdownButton,
+  Dropdown,
+} from "react-bootstrap";
 import "../App.css";
 
 const MyNavbar = () => {
@@ -11,16 +19,57 @@ const MyNavbar = () => {
             <input className="input-field" type="text" placeholder="Search" />
           </Col>
           <Col md={5} className="col-md5-icons">
-            <img src="/images/nav-home.svg" alt="" />
-            <img src="/images/nav-network.svg" alt="" />
-            <img src="/images/nav-jobs.svg" alt="" />
-            <img src="/images/nav-messaging.svg" alt="" />
-            <img src="/images/nav-notifications.svg" alt="" />
-            <img src="/images/down-icon.svg" alt="" />
+            <div>
+              <img src="/images/nav-home.svg" alt="" />
+              <p>Home</p>
+            </div>
+            <div>
+              <img src="/images/nav-network.svg" alt="" />
+              <p>My Network</p>
+            </div>
+            <div>
+              <img src="/images/nav-jobs.svg" alt="" />
+              <p>Jobs</p>
+            </div>
+            <div>
+              <img src="/images/nav-messaging.svg" alt="" />
+              <p>Messaging</p>
+            </div>
+            <div>
+              <img src="/images/nav-notifications.svg" alt="" />
+              <p>Notifications</p>
+            </div>
+              <ButtonGroup>
+             <div id="user-wrapper">
+                <img src="/images/user.svg" alt="" id="user-img"/>
+               
+                  <DropdownButton
+                    variant="light"
+                    title="Me"
+                    id="bg-nested-dropdown"
+                  >
+                    <Dropdown.Item eventKey="1">Dropdown link</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Dropdown link</Dropdown.Item>
+                  </DropdownButton>
+             </div>
+              </ButtonGroup>
           </Col>
           <Col md={2}>
             <Col>
-              <img src="/images/nav-work.svg" alt="" />
+                <ButtonGroup>
+             <div id="user-wrapper">
+                <img src="/images/nav-work.svg" alt=""/>
+               
+                  <DropdownButton
+                    variant="light"
+                    title="Work"
+                    id="bg-nested-dropdown"
+                  >
+                    <Dropdown.Item eventKey="1">Dropdown link</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Dropdown link</Dropdown.Item>
+                  </DropdownButton>
+             </div>
+              </ButtonGroup>
             </Col>
             <span>Get Sales Navigator</span>
           </Col>
