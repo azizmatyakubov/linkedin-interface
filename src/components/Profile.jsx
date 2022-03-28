2c0eeaa53124c25fa1235cf8e0bb0dddc5fbb6ba
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-03-28 22:10:26 +0100

    done with jumbotron

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import styled from "styled-components";
+import MyJumbotrons from "./MyJumbotron";
 
 const Profile = () => {
   return (
     <Container>
       <Row>
         <Col md={8}>
-          <Right>LEFT SECTION</Right>
+          <MyJumbotrons />
         </Col>
-        <Col md={4}>
-          <Left>RIGHT SECTION</Left>
-        </Col>
+        <Col md={4}></Col>
       </Row>
       <Footer>footer</Footer>
     </Container>
@@ -19,14 +18,6 @@ const Profile = () => {
 };
 
 export default Profile;
-
-const Right = styled.div`
-  background-color: green;
-`;
-
-const Left = styled.div`
-  background-color: #002fff;
-`;
 
 const Footer = styled.div`
   background-color: pink;
