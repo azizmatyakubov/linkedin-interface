b667df7879866f31d2470490c97e243a0e29b46d
AZIZ <a.matyaqubov1471@gmail.com>
2022-03-28 10:59:28 +0200

    create template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Profile from './components/Profile';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-     
+      <Profile />
+      <BrowserRouter>
+      {/* Navbar  */}
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import styled from "styled-components";
+
+const Profile = () => {
+  return (
+    <Container>
+      <Row>
+        <Col md={8}>
+          <Right>LEFT SECTION</Right>
+        </Col>
+        <Col md={4}>
+          <Left>RIGHT SECTION</Left>
+        </Col>
+      </Row>
+      <Footer>footer</Footer>
+    </Container>
+  );
+};
+
+export default Profile;
+
+const Right = styled.div`
+  background-color: green;
+`;
+
+const Left = styled.div`
+  background-color: #002fff;
+`;
+
+const Footer = styled.div`
+  background-color: pink;
+`;
