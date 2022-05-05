86198621e5dadd78f711b2be99f65cfab5eb9831
Lucai994 <luca.muratori.94@hotmail.it>
2022-05-05 11:31:34 +0200

    commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -44,7 +44,7 @@ const Profile = (props) => {
 
   const fetchMyExperience = async () => {
     const response = await fetch(
-      "https://striveschool-api.herokuapp.com/api/profile/6242131ed339840015c883bb/experiences",
+      "https://linkedin-backend-01.herokuapp.com/profile/6242131ed339840015c883bb/experiences",
       {
         headers: {
           "Content-Type": "application/json",
